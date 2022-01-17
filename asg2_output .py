f1d801191db67d691fcb0d223f4a905b893271a8
Mehul-Mangla <88623934+Mehul-Mangla@users.noreply.github.com>
2022-01-17 16:32:38 +0530

    Add files via upload

---
+++ asg2_output .py
@@ -0,0 +1,47 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:\Mehul\Python\asg2_21105043 .py ==================
+           Question 1
+
+Python is a case sensitive language
+Length=  35
+Reverse=  egaugnal evitisnes esac a si nohtyP
+Python is object oriented language
+Index=  10
+Pythonisacasesensitivelanguage
+
+           Question 2
+
+Hey, MEHUL MANGLA  Here!
+My SID is  21105043
+I am from  ECE  department and my CGPA is  10
+
+           Question 3
+
+a&b=  8
+a|b=  58
+a^b=  50
+a<<2=  224
+b<<2=  40
+a>>2=  14
+b>>4=  0
+
+           Question 4
+
+1st number=? 23
+2nd number=? 34
+3rd number=? 45
+Largest number =  45
+
+           Question 5
+
+Enter a sentence my name is Mehul
+Yes
+
+           Question 6
+
+Enter first side length 5
+Enter second side length 8
+Enter third side length 7
+Yes
