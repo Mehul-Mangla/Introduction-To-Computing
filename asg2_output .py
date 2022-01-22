fc377267e7c0f19cdf060b62f5db7030eb68110e
Mehul-Mangla <88623934+Mehul-Mangla@users.noreply.github.com>
2022-01-22 08:44:46 +0530

    Update asg2_output .py

--- asg2_output .py
+++ asg2_output .py
@@ -1,7 +1,7 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-================== RESTART: D:\Mehul\Python\asg2_21105043 .py ==================
+============== RESTART: D:\Mehul\College\Python\asg2_21105043 .py ==============
            Question 1
 
 Python is a case sensitive language
@@ -14,8 +14,8 @@ Pythonisacasesensitivelanguage
            Question 2
 
 Hey, Mehul Mangla here!
-My SID is  21105043
-I am from  ECE  department and my CGPA is  10
+My SID is 21105043
+I am from ECE department and my CGPA is 9.900000
 
            Question 3
 
@@ -29,19 +29,19 @@ b>>4=  0
 
            Question 4
 
-1st number=? 23
-2nd number=? 34
+1st number=? 34
+2nd number=? 23
 3rd number=? 45
-Largest number =  45
+Greatest number:  45
 
            Question 5
 
-Enter a sentence my name is mehul
+Enter a sentence my name is Mehul
 Yes
 
            Question 6
 
-Enter first side length 3
-Enter second side length 4
-Enter third side length 6
+Enter first side length 6
+Enter second side length 3
+Enter third side length 5
 Yes
