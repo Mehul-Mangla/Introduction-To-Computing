f70743b997ae816d46908ff8e45657e0f900ae38
Mehul Mangla <88623934+Mehul-Mangla@users.noreply.github.com>
2022-02-25 20:12:20 +0530

    Add files via upload

---
+++ asg4_output .py
@@ -0,0 +1,104 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: D:\Mehul\College\Python\Assignments\Asg 4\asg4_21105043 .py =====
+
+           Question 1
+
+Enter the number of disks : 3
+
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+           Question 2
+
+Enter the number of rows : 5
+
+By Loop Method - 
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+By Recursion Method - 
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+           Question 3
+
+Enter the first number : 120
+Enter the second number : -7
+
+Quotient is :  -18
+Remainder is :  6
+
+Q3 (a)
+True
+
+Q3 (b)
+All values are non-zero
+
+Q3 (c)
+[4, 5, 6, -18, 6]
+Values greater than 4 are :  [5, 6, 6]
+
+Q3 (d)
+{4, 5, 6, -18}
+
+Q3 (e)
+Immutable set :  frozenset({4, 5, 6, -18})
+
+Q3 (f)
+6
+
+           Question 4
+
+Student created
+Student created
+Student created
+
+Student deleted
+
+           Question 5
+
+Record of all employees
+
+Employee Name :  Mehak
+Employee Salary :  40000
+
+
+Employee Name :  Ashok
+Employee Salary :  50000
+
+
+Employee Name :  Viren
+Employee Salary :  60000
+
+
+Q5 (a)
+Updated record of employee Mehak
+
+Employee Name :  Mehak
+Employee Salary :  70000
+
+
+Q5 (b)
+Record of Vivek deleted
+
+           Question 6
+
+Friend 1, please enter your word : cinema
+Friend 2, please enter your word : iceman
+
+Your friendship is real
